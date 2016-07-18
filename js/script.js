73e5396f04821cb94b3d65dd7be2555d9281cf79
riaz khan <khanivore0311@gmail.com>
2016-07-18 16:54:49 -0500

    Final project updated correctly Project 1
    I am not sure what happened in previous edits, but this is the final
    project.

--- js/script.js
+++ js/script.js
@@ -9,66 +9,24 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 	//TODO RANDOMIZE QUOTE INDEX
 	var randomNumber = Math.floor(Math.random() * 5);
 	// selects a random quote object from the quotes array
-<<<<<<< HEAD
     var quote = quotes[randomNumber];
-    // returns the randomly selected quote object
-	return quote;	
-=======
-    var quote = quotes[2];
     // returns the randomly selected quote object
 	return quote;	
 }
 
+
+
 //Create a function named printQuote which follows these rules:
-
-function printQuote (getRandomQuote) {
- var getRandomQuoteVar = getRandomQuote; //getRandomQuote () 
- var listHTML = '<ol>';
-  for ( var i = 0; i < quotes.length; i += 1) {
-    listHTML += '<li>' + quotes[i][0] + ' by ' + quotes[i][1] + '</li>';
-  }
-  listHTML += '</ol>';
-  //print(listHTML);
-  console.log(listHTML);
-  }
-
-//printQuote calls the getRandomQuote function and stores the returned quote object in a variable	
-printQuote (getRandomQuote);
->>>>>>> origin/master
-	
-
-//}
-
-/*
-
-
-//<p class="quote"> [quote here] </p> <p class="source"> [source here] <span class="citation"> [citation here] </span> <span class="year"> [year here] </span> </p>
-
-//var sentence = document.getElementByClassName(quote);
-//console.log(sentence);
-
-console.log(getRandomQuote ());
-
-/*
-Create a function named printQuote which follows these rules:
-printQuote calls the getRandomQuote function and stores the returned quote object in a variable
-printQuote constructs a string using the different properties of the quote object using the following HTML template: <p class="quote"> [quote here] </p> <p class="source"> [source here] <span class="citation"> [citation here] </span> <span class="year"> [year here] </span> </p>
-printQuote doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
-printQuote displays the final HTML string to the page. You can use the following JS snippet to accomplish that: document.getElementById('quote-box').innerHTML
-*/
-
 function printQuote () {
- var getRandomQuoteVar = getRandomQuote();
+ //printQuote calls the getRandomQuote function and stores the returned quote object in a variable
+	var getRandomQuoteVar = getRandomQuote();
 	console.log(getRandomQuoteVar);
-	 document.getElementById('quote-box').innerHTML = 
+//get the ID called 'quote-box' from the document(index.html) and change the HTML in the quote-box to...	
+	document.getElementById('quote-box').innerHTML = 
+//the below.
 	'<p class="quote">' + getRandomQuoteVar.quote + '</p>' + 
 	'<p class="source">' + getRandomQuoteVar.name + '</p>'; 
 }
 
 
 
-
-<<<<<<< HEAD
-=======
-document.getElementById('quote-box').innerHTML = quotes[0].quote;
->>>>>>> origin/master
--- js/test.js
+++ js/test.js
@@ -1,33 +1,30 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-//alert("Hello world");
-//var howAreYou = prompt("How are you doing today?");
-//document.write(howAreYou);
-
-
+//Create a function named getRandomQuote which:
+//selects a random quote object from the quotes array
+//returns the randomly selected quote object
  function getRandomQuote () {
 	//TODO RANDOMIZE QUOTE INDEX
-	
+	var randomNumber = Math.floor(Math.random() * 5);
 	// selects a random quote object from the quotes array
 <<<<<<< HEAD
-    var quote = quotes[2].name;
+    var quote = quotes[randomNumber];
+    // returns the randomly selected quote object
+	return quote;	
 =======
     var quote = quotes[2];
->>>>>>> origin/master
     // returns the randomly selected quote object
 	return quote;	
 }
 
+//Create a function named printQuote which follows these rules:
 
-<<<<<<< HEAD
-console.log(getRandomQuote ());
-
-
-
-
-function printQuote () {
+function printQuote (getRandomQuote) {
  var getRandomQuoteVar = getRandomQuote; //getRandomQuote () 
  var listHTML = '<ol>';
-  for ( var i = 0; i < quotes; i += 1) {
+  for ( var i = 0; i < quotes.length; i += 1) {
     listHTML += '<li>' + quotes[i][0] + ' by ' + quotes[i][1] + '</li>';
   }
   listHTML += '</ol>';
@@ -36,27 +33,42 @@ function printQuote () {
   }
 
 //printQuote calls the getRandomQuote function and stores the returned quote object in a variable	
-printQuote ();
+printQuote (getRandomQuote);
+>>>>>>> origin/master
+	
 
-//
+//}
 
-var message = '';
-var student;
+/*
 
-function print(message) {
-  var outputDiv = document.getElementById('output');
-  outputDiv.innerHTML = message;
-}
 
-for (var i = 0; i < students.length; i += 1) {
-  student = students[ i ];
-  message += '<h2>Student: ' + student.name + '</h2>';
-  message += '<p>Track: ' + student.track + '</p>';
-  message += '<p>Points: ' + student.points + '</p>';
-  message += '<p>Achievements: ' + student.achievements + '</p>';
-  
-}
-print(message);
-=======
+//<p class="quote"> [quote here] </p> <p class="source"> [source here] <span class="citation"> [citation here] </span> <span class="year"> [year here] </span> </p>
+
+//var sentence = document.getElementByClassName(quote);
+//console.log(sentence);
+
 console.log(getRandomQuote ());
+
+/*
+Create a function named printQuote which follows these rules:
+printQuote calls the getRandomQuote function and stores the returned quote object in a variable
+printQuote constructs a string using the different properties of the quote object using the following HTML template: <p class="quote"> [quote here] </p> <p class="source"> [source here] <span class="citation"> [citation here] </span> <span class="year"> [year here] </span> </p>
+printQuote doesn't add a <span class="citation"> for a missing citation or a <span class="year"> if the year property is missing
+printQuote displays the final HTML string to the page. You can use the following JS snippet to accomplish that: document.getElementById('quote-box').innerHTML
+*/
+
+function printQuote () {
+ var getRandomQuoteVar = getRandomQuote();
+	console.log(getRandomQuoteVar);
+	 document.getElementById('quote-box').innerHTML = 
+	'<p class="quote">' + getRandomQuoteVar.quote + '</p>' + 
+	'<p class="source">' + getRandomQuoteVar.name + '</p>'; 
+}
+
+
+
+
+<<<<<<< HEAD
+=======
+document.getElementById('quote-box').innerHTML = quotes[0].quote;
 >>>>>>> origin/master
